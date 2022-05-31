c3bbe5e48f914eb5948de95837b12d702eb1e7ff
Rinat Mukhometzianov <25082858+rmukh@users.noreply.github.com>
2022-05-31 09:45:54 -0400

    basic bids copy, need to copy json and bval bvec files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from shared_core.project_parser import Parser
 from shared_core.dicom_conversion import DICOM
+from shared_core.bids_checks import BIDS
 from nipype.interfaces.io import BIDSDataGrabber
 
 parser = Parser()
@@ -12,27 +13,30 @@ print("Subjects: {}".format(subjects))
 
 dicom = DICOM(args, subjects)
 out_folder = dicom.run_conversion()
-print("Output folder: {}".format(out_folder))
 
-bg = BIDSDataGrabber()
-bg.inputs.base_dir = ''
-bg.inputs.output_query = {
-    "T1w": {
-        "datatype": "anat",
-        "suffix": "T1w",
-        "extension": ["nii", ".nii.gz"],
-    },
-    "T2w": {
-        "datatype": "anat",
-        "suffix": "T2w",
-        "extension": ["nii", ".nii.gz"],
-    },
-    "dwi": {
-        "datatype": "dwi",
-        "suffix": "dwi",
-        "extension": ["nii", ".nii.gz"],
-    },
-}
-bg.inputs.raise_on_empty = False
-results = bg.run()
+bids = BIDS(out_folder, subjects)
+bids.run_check()
+if bids.is_bids():
+    print("BIDS compliant!")
 
+# bg = BIDSDataGrabber()
+# bg.inputs.base_dir = test_path
+# bg.inputs.output_query = {
+#     "T1w": {
+#         "datatype": "anat",
+#         "suffix": "T1w",
+#         "extension": ["nii", ".nii.gz"],
+#     },
+#     "T2w": {
+#         "datatype": "anat",
+#         "suffix": "T2w",
+#         "extension": ["nii", ".nii.gz"],
+#     },
+#     "dwi": {
+#         "datatype": "dwi",
+#         "suffix": "dwi",
+#         "extension": ["nii", ".nii.gz"],
+#     },
+# }
+# bg.inputs.raise_on_empty = False
+# results = bg.run()
---
+++ shared_core/bids_checks.py
@@ -0,0 +1,225 @@
+import json
+import os
+import shutil
+
+from bids.layout import BIDSLayout
+from bids.exceptions import BIDSValidationError
+
+
+class BIDS:
+    def __init__(self, work_dir, subjects):
+        self.layout = None
+        self.convert_to_bids = False
+        self.bids_ok = False
+        self.n_subjects_bids = 0
+
+        self.work_dir = work_dir
+        self.subjects = subjects
+
+        self.nifti_files = self.find_nifti_files(self.work_dir)
+
+    @staticmethod
+    def find_nifti_files(fld):
+        # find all NIFTI files
+        found = []
+        for root, _, files in os.walk(fld):
+            for file in files:
+                if file.endswith(".nii.gz") or file.endswith(".nii"):
+                    found.append(os.path.join(root, file))
+        return found
+
+    def check_bids(self):
+        self.convert_to_bids = False
+        # do checks
+        try:
+            self.layout = BIDSLayout(self.work_dir, validate=True)
+        except BIDSValidationError as e:
+            if "dataset_description.json" in str(e):
+                print("'dataset_description.json' is missing from project root. "
+                      "Every valid BIDS dataset must have this file.")
+                print("Probably your dataset is not valid BIDS dataset.")
+                print("If your dataset is valid BIDS dataset, please create this file and try again.")
+                print("Otherwise, do you want to try this pipeline to convert your dataset into a BIDS format?")
+                yn = input("[y/n] ")
+                if yn.lower() == "y":
+                    self.convert_to_bids = True
+                else:
+                    print("Please make your dataset valid BIDS dataset and try again.")
+                    exit(0)
+
+        if not self.convert_to_bids:
+            self.n_subjects_bids = len(self.layout.get_subjects())
+
+        if self.n_subjects_bids == 0 and len(self.nifti_files) > 1:
+            print("We found NIFTI files in the provided input directory, but no subjects in the BIDS layout.")
+            print("Do you want to try this pipeline to convert your dataset into a BIDS format?")
+            yn = input("[y/n] ")
+            if yn.lower() == "y":
+                self.convert_to_bids = True
+            else:
+                print("Please make your dataset valid BIDS dataset and try again.")
+                exit(0)
+        elif self.n_subjects_bids == 0 and len(self.nifti_files) < 2:
+            print("We found no NIFTI or DICOM files!")
+            print("Makesure that your input directory is not empty and rerun this pipeline.")
+            exit(0)
+        elif self.n_subjects_bids < len(self.subjects):
+            print("Number of subjects in BIDS format is less than number of detected subjects in the input directory.")
+            print("Please make sure that you have the correct BIDS layout.")
+            print("If you want to continue processing only the detected subjects, please answer 'y'.")
+            bids_ask = input("[y/n] ")
+            if bids_ask.lower() == "y":
+                print("Continue processing only the detected subjects.")
+                self.bids_ok = True
+            else:
+                print("Please make sure that you have the correct BIDS layout.")
+                exit(0)
+        elif self.n_subjects_bids > len(self.subjects):
+            print("Number of subjects in BIDS format is more than number of detected subjects in the input directory.")
+            print("Please make sure that you have the correct BIDS layout!")
+            exit(0)
+        else:
+            print("Number of subjects in BIDS format is equal to number of detected subjects in the input directory.")
+            print("OK BIDS format.")
+            self.bids_ok = True
+
+    def run_check(self):
+        self.check_bids()
+
+        if self.convert_to_bids:
+            print("Converting to BIDS format...")
+            print("Do you want to reorganize your dataset into BIDS format "
+                  "within the input folder or specify a new one?")
+            print("If you want to use the input folder, please answer 'y', "
+                  "if you want another one, please specify the FULL absolute path.")
+            option = input()
+            if option.lower() == "y":
+                bids_folder = self.work_dir
+            else:
+                bids_folder = os.path.abspath(option)
+                try:
+                    os.makedirs(bids_folder, exist_ok=True)
+                except OSError:
+                    print("The specified path is not a valid directory.")
+                    print("Please rerun and specify valid directory.")
+                    exit(0)
+
+            print("What is the name of your dataset?")
+            dataset_name = input("[My Dataset by default]: ")
+            if dataset_name == "":
+                dataset_name = "My Dataset"
+
+            with open(os.path.join(bids_folder, "dataset_description.json"), "w") as f:
+                f.write(json.dumps({"Name": dataset_name, "BIDSVersion": "1.6.0"}))
+            print("We created a dataset_description.json file in the output directory {}"
+                  "".format(os.path.join(bids_folder, "dataset_description.json")))
+
+            print("We need to identify the imaging modalities in your dataset.")
+            print("* you can specify multiple patters, separated by commas.")
+            is_t1, t1_patterns = self.ask_modality("T1", "IR-SPGR, T1-SPGR, T1-FLAIR")
+            is_t2, t2_patterns = self.ask_modality("T2", "T2_Star, inplaneT2")
+            is_dwi, dwi_patterns = self.ask_modality("DWI", "DTI, DWI, DWI_b1000")
+
+            new_subject_names = {}
+            subjects_digits_len = len(str(len(self.subjects)))
+            if subjects_digits_len == 1:
+                subjects_digits_len = 2
+            for ind, subj in enumerate(self.subjects):
+                print(f"Processing subject {subj}")
+                new_ind = str(ind).zfill(subjects_digits_len)
+                new_sub = f"sub-{new_ind}"
+
+                # fill mapping dict
+                new_subject_names[new_sub] = subj
+
+                # create subject folder structure with new name
+                new_sub_folder = os.path.join(bids_folder, new_sub)
+                os.makedirs(new_sub_folder, exist_ok=True)
+                os.makedirs(os.path.join(new_sub_folder, "anat"), exist_ok=True)
+                os.makedirs(os.path.join(new_sub_folder, "dwi"), exist_ok=True)
+
+                # find MR images
+                all_locals_t1 = []
+                all_locals_t2 = []
+                all_locals_dwi = []
+
+                for f in self.nifti_files:
+                    t1 = self.find_modality_for_subject(is_t1, t1_patterns, subj, f)
+                    all_locals_t1.append(t1) if t1 is not None else None
+                    t2 = self.find_modality_for_subject(is_t2, t2_patterns, subj, f)
+                    all_locals_t2.append(t2) if t2 is not None else None
+                    dwi = self.find_modality_for_subject(is_dwi, dwi_patterns, subj, f)
+                    all_locals_dwi.append(dwi) if dwi is not None else None
+
+                # copy images to new folder in BIDS format
+                fn_new_t1 = new_sub + "_T1w.nii.gz"
+                fn_new_t2 = new_sub + "_T2w.nii.gz"
+                fn_new_dwi = new_sub + "_dwi.nii.gz"
+
+                self.copy_2_bids(all_locals_t1, "anat", new_sub_folder, subj, fn_new_t1)
+                self.copy_2_bids(all_locals_t2, "anat", new_sub_folder, subj, fn_new_t2)
+                self.copy_2_bids(all_locals_dwi, "dwi", new_sub_folder, subj, fn_new_dwi)
+
+                # clean up
+                shutil.rmtree(os.path.join(bids_folder, subj))
+
+            print("T1 images will have a suffix '_T1w', T2 images will have a suffix '_T2w'"
+                  " and DWI images will have a suffix '_dwi'.")
+            with open(os.path.join(bids_folder, "mapping.json"), "w") as f:
+                f.write(json.dumps(new_subject_names))
+            print("We created a mapping.json file in the output directory.{}"
+                  "".format(os.path.join(bids_folder, "mapping.json")))
+            print("You can use it to map the subject names from the BIDS format to the original names.")
+            self.check_bids()
+
+    @staticmethod
+    def ask_modality(modality, pattern_examples):
+        is_modality = False
+        patterns = None
+
+        print(f"Do you have {modality} images?")
+        ask = input("[y/n] ")
+        if ask.lower() == "y":
+            is_modality = True
+            print("Please specify any distinct pattern either in folder or file name")
+            print(f"we can use to identify {modality} images. It can be {pattern_examples}, etc.")
+            patterns = input(f"[{modality} by default]: ")
+            if patterns == "":
+                patterns = f"{modality}"
+        return is_modality, patterns
+
+    @staticmethod
+    def find_modality_for_subject(is_used, patterns, subj, f):
+        if is_used:
+            for pattern in patterns.split(","):
+                if pattern.strip() in f and subj in f:
+                    return f
+        return None
+
+    @staticmethod
+    def copy_2_bids(all_locals, data_type, new_sub_folder, subj, fn_new):
+        if len(all_locals) == 1:
+            shutil.copy(all_locals[0], os.path.join(new_sub_folder, data_type, fn_new))
+        elif len(all_locals) == 0:
+            print(f"No T1 images found for subject {subj}!")
+            print("You would need to fix this manually, "
+                  "otherwise this subject won't be processed in the pipeline!")
+        else:
+            print(f"Multiple T1 images found for subject {subj}!")
+            print("Please specify the index of which one you want to use.")
+            for ind, f in enumerate(all_locals):
+                print(f"{ind}: {f}")
+            print("Please enter the index of the image you want to use.")
+            while True:
+                index_to_use = input()
+                try:
+                    index_to_use = int(index_to_use)
+                except ValueError:
+                    print("Enter valid int value")
+                else:
+                    break
+
+            shutil.copy(all_locals[index_to_use], os.path.join(new_sub_folder, data_type, fn_new))
+
+    def is_bids(self):
+        return self.bids_ok
