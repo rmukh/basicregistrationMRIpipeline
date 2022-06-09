49f7a39a733e04673bf20835c02d2e38a63089a0
Rinat Mukhometzianov <25082858+rmukh@users.noreply.github.com>
2022-06-08 21:58:06 -0400

    improved main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import datetime
 import os.path as op
 import shutil
 
@@ -7,87 +6,90 @@ from nipype.pipeline.engine import Workflow
 from shared_core.project_parser import Parser
 from shared_core.dicom_conversion import DICOM
 from shared_core.bids_checks import BIDS
+from shared_core.utils import continuously_ask_user_yn, execution_time
 
 from modules.data_handler import data_source, bids_grabber, data_sink, mif_input_combiner, get_meta_parameters
 from modules.preprocesses import preprocess_dwi_workflow, preprocess_anat_workflow
 from modules.registration import registration_workflow
 
-now = datetime.datetime.now()
 
-parser = Parser()
-args = parser.parse()
+@execution_time
+def main():
+    parser = Parser()
+    args = parser.parse()
 
-# detect the subjects in the input directory
-subjects = parser.get_subjects()
-print("Found {} subjects".format(len(subjects)))
-print("Subjects: {}".format(subjects))
+    # detect the subjects in the input directory
+    subjects = parser.get_subjects()
+    print("Found {} subjects".format(len(subjects)))
+    print("Subjects: {}".format(subjects))
 
-dicom = DICOM(args, subjects)
-out_folder = dicom.run_conversion()
+    # IF DICOM files are found, convert them to NIFTI
+    # and create a BIDS directory structure for the data
+    dicom = DICOM(args, subjects)
+    out_folder = dicom.run_conversion()
 
-bids = BIDS(out_folder, subjects)
-bids.run_check()
-if bids.is_bids():
-    print("BIDS compliant dataset!")
-subjects = bids.get_bids_subjects()
+    # check if the BIDS directory structure is valid
+    bids = BIDS(out_folder, subjects)
+    bids.run_check()
+    subjects = bids.get_bids_subjects()
 
-out_folder = bids.get_work_dir()
-scrap_directory = op.join(out_folder, "scrap")
+    # Get final output folder
+    out_folder = bids.get_work_dir()
+    scrap_directory = op.join(out_folder, "scrap")
 
-# IO
-source_iterator = data_source(subjects)
-bids_source = bids_grabber(out_folder)
-sink = data_sink(out_folder, args.output)
+    # IO nodes
+    source_iterator = data_source(subjects)
+    bids_source = bids_grabber(out_folder)
+    sink = data_sink(out_folder, args.output)
 
-# main processing modules (workflows)
-combine_dwi = mif_input_combiner(args.ncpus)
-combine_dwi.base_dir = scrap_directory
+    # define main processing modules (workflows)
+    combine_dwi = mif_input_combiner(args.ncpus)
+    meta_parameters = get_meta_parameters()
+    preprocess_dwi = preprocess_dwi_workflow(args.ncpus)
+    preprocess_anat = preprocess_anat_workflow(args.ncpus)
+    registration = registration_workflow(args.ncpus)
 
-meta_parameters = get_meta_parameters()
-preprocess_dwi = preprocess_dwi_workflow(args.ncpus)
-preprocess_anat = preprocess_anat_workflow(args.ncpus)
-registration = registration_workflow(args.ncpus)
+    print(f"Starting workflow with {args.ncpus} threads")
+    wf = Workflow(name="pipeline_registration", base_dir=scrap_directory)
+    if args.debug:
+        wf.config['execution'] = {'stop_on_first_crash': 'True'}
 
-print(f"Starting workflow with {args.ncpus} threads")
-wf = Workflow(name="pipeline_registration", base_dir=scrap_directory)
-if args.debug:
-    wf.config['execution'] = {'stop_on_first_crash': 'True'}
+    # define the workflow with the modules and correctly define interconnections
+    wf.connect([
+        (source_iterator, bids_source, [("subject", "subject")]),
+        (bids_source, combine_dwi, [("dwi", "inputnode.dwi"),
+                                    ("bvec", "inputnode.bvec"),
+                                    ("bval", "inputnode.bval")]),
+        (bids_source, meta_parameters, [("dwi_meta", "meta_path")]),
+        (combine_dwi, preprocess_dwi, [("outputnode.dwi", "inputnode.dwi")]),
+        (meta_parameters, preprocess_dwi, [("pe", "inputnode.pe"),
+                                           ("rt", "inputnode.rt")]),
+        (bids_source, preprocess_anat, [("T1w", "inputnode.t1"),
+                                        ("T2w", "inputnode.t2")]),
 
-wf.connect([
-    (source_iterator, bids_source, [("subject", "subject")]),
-    (bids_source, combine_dwi, [("dwi", "inputnode.dwi"),
-                                ("bvec", "inputnode.bvec"),
-                                ("bval", "inputnode.bval")]),
-    (bids_source, meta_parameters, [("dwi_meta", "meta_path")]),
-    (combine_dwi, preprocess_dwi, [("outputnode.dwi", "inputnode.dwi")]),
-    (meta_parameters, preprocess_dwi, [("pe", "inputnode.pe"),
-                                       ("rt", "inputnode.rt")]),
-    (bids_source, preprocess_anat, [("T1w", "inputnode.t1"),
-                                    ("T2w", "inputnode.t2")]),
+        (preprocess_dwi, registration, [("outputnode.mean_b0", "inputnode.mean_b0"),
+                                        ("outputnode.dwi_nifti", "inputnode.dwi_nifti")]),
+        (preprocess_anat, registration, [("outputnode.t1", "inputnode.t1"),
+                                         ("outputnode.t2", "inputnode.t2")]),
 
-    (preprocess_dwi, registration, [("outputnode.mean_b0", "inputnode.mean_b0"),
-                                    ("outputnode.dwi_nifti", "inputnode.dwi_nifti")]),
-    (preprocess_anat, registration, [("outputnode.t1", "inputnode.t1"),
-                                     ("outputnode.t2", "inputnode.t2")]),
-    
-    (preprocess_anat, sink, [("outputnode.t1", "anat.@t1")]),
-    (registration, sink, [("outputnode.dwi", "dwi.@dwi"),
-                          ("outputnode.t2", "anat.@t2")]),
-    (preprocess_dwi, sink, [("outputnode.bvec", "dwi.@dwi_bvec"),
-                            ("outputnode.bval", "dwi.@dwi_bval")])
-])
+        (preprocess_anat, sink, [("outputnode.t1", "anat.@t1")]),
+        (registration, sink, [("outputnode.dwi", "dwi.@dwi"),
+                              ("outputnode.t2", "anat.@t2")]),
+        (preprocess_dwi, sink, [("outputnode.bvec", "dwi.@dwi_bvec"),
+                                ("outputnode.bval", "dwi.@dwi_bval")])
+    ])
 
-# Run
-wf.run()
+    # Run main workflow
+    wf.run()
 
-print("Total time: ", str(datetime.datetime.now() - now))
-if not args.final_cleanup:
-    yn = input("Do you want to delete the temporary directory? [y/n] ")
-    if yn.lower() == "y":
-        args.final_cleanup = True
-    else:
-        args.final_cleanup = False
+    if not args.final_cleanup:
+        args.final_cleanup = continuously_ask_user_yn("Do you want to delete the temporary directory?")
 
-if args.final_cleanup:
-    print("Deleting temporary directory")
-    shutil.rmtree(scrap_directory)
+    # if the user selected deletion, then delete the temporary directory
+    if args.final_cleanup:
+        print("Deleting temporary directory")
+        shutil.rmtree(scrap_directory)
+
+
+if __name__ == "__main__":
+    main()
---
+++ shared_core/utils.py
@@ -0,0 +1,32 @@
+import functools
+import datetime
+
+
+def continuously_ask_user_yn(question: str) -> bool:
+    """
+    Continuously ask the user for a valid answer until one is given.
+    """
+    question = question + " [y/n]: "
+    while True:
+        answer = input(question)
+        if answer in ["y", "n"]:
+            if answer == "y":
+                return True
+            else:
+                return False
+        else:
+            print("Invalid answer, please try again")
+
+
+def execution_time(func):
+    """
+    Decorator to measure the execution time of a function.
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = datetime.datetime.now()
+        result = func(*args, **kwargs)
+        print("Total run time: ", str(datetime.datetime.now() - start))
+        return result
+    return wrapper
